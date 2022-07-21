a07c3ea326083d22abba4882db2335d61599374f
Tatiana Sanabria <tata.sanabria@gmail.com>
2022-07-20 20:22:05 -0500

    Es la primera vez que subo cambios a mi repo, ya tengo funcionalidad de cifrado e implementacion de mi css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
 
+
+const encodeButton = document.getElementById("Cipher");
+const decodeButton = document.getElementById("Decipher")
+const finalText = document.getElementById("message2");
+
+encodeButton.addEventListener("click" , () => {
+    const firstText = document.getElementById("Message1").value;
+    const offset = document.getElementById("displacement").value;
+    finalText.value = cipher.encode(offset, firstText);
+});
+decodeButton.addEventListener("click" , () => {
+    const firstText = document.getElementById("Message1").value;
+    const offset = document.getElementById("displacement").value;
+    finalText.value = cipher.decode(offset, firstText);
+});
 console.log(cipher);
